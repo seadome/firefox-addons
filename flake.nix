f667233a369fae384e60714d3e20e842a8a9bdfc
Chris Montgomery <chris@cdom.io>
2023-01-02 16:21:01 -0500

    feat: rework devshell with mission-control flake module

--- flake.nix
+++ flake.nix
@@ -1,16 +1,48 @@
 {
   description = "firefox addons manifest";
-  inputs.nixpkgs.url = "github:numtide/nixpkgs-unfree";
-  inputs.nixpkgs.inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
-  inputs.mozilla-addons-to-nix.url = "sourcehut:~rycee/mozilla-addons-to-nix";
-  outputs = inputs @ {flake-parts, ...}:
+
+  inputs = {
+    nixpkgs.url = "github:numtide/nixpkgs-unfree";
+    nixpkgs.inputs.nixpkgs.url = "nixos-unstable";
+    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+
+    mozilla-addons-to-nix.url = "sourcehut:~rycee/mozilla-addons-to-nix";
+  };
+
+  outputs = inputs @ {flake-parts, ...}: let
+    l = inputs.nixpkgs.lib // builtins;
+  in
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [./devShells ./packages];
+      imports = [
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+        ./src
+      ];
       systems = ["aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
-      perSystem = {inputs', ...}: {
+      perSystem = {
+        inputs',
+        config,
+        pkgs,
+        ...
+      }: {
         _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
-        formatter = inputs'.nixpkgs.legacyPackages.alejandra;
+        formatter = pkgs.alejandra;
+        devShells.default = config.mission-control.installToDevShell (pkgs.mkShell {});
+        mission-control.scripts = {
+          update = {
+            description = "Update the Firefox addons from the addons.json manifest file.";
+            exec = ''
+              ${l.getExe inputs'.mozilla-addons-to-nix.packages.default} \
+                ./src/addons.json \
+                ./src/addons.generated.nix
+            '';
+            category = "actions";
+          };
+        };
       };
     };
 }
