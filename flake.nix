612f3e1c650974ebdd41921aa3304de3abb7a4fb
Chris Montgomery <chris@cdom.io>
2023-02-05 16:53:52 -0500

    feat: use `numtide/devshell`

--- flake.nix
+++ flake.nix
@@ -7,21 +7,14 @@
     nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-root.url = "github:srid/flake-root";
-    mission-control.url = "github:Platonic-Systems/mission-control";
+    devshell.url = "github:numtide/devshell";
 
     mozilla-addons-to-nix.url = "sourcehut:~rycee/mozilla-addons-to-nix";
   };
 
-  outputs = inputs @ {flake-parts, ...}: let
-    l = inputs.nixpkgs.lib // builtins;
-  in
+  outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [
-        inputs.flake-root.flakeModule
-        inputs.mission-control.flakeModule
-        ./src
-      ];
+      imports = [./src];
       systems = ["aarch64-linux" "aarch64-darwin" "x86_64-darwin" "x86_64-linux"];
       perSystem = {
         inputs',
@@ -31,17 +24,20 @@
       }: {
         _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
         formatter = pkgs.alejandra;
-        devShells.default = config.mission-control.installToDevShell (pkgs.mkShell {});
-        mission-control.scripts = {
-          update = {
-            description = "Update the Firefox addons from the addons.json manifest file.";
-            exec = ''
-              ${l.getExe inputs'.mozilla-addons-to-nix.packages.default} \
-                ./src/addons.json \
-                ./src/addons.generated.nix
-            '';
-            category = "actions";
-          };
+        # TODO: build all <https://github.com/NixOS/nix/issues/7165>
+        # packages.all = pkgs.symlinkJoin {
+        #     name = "all";
+        #     paths = with code; [ app wasm ];
+        #   };
+        devShells.default = config.devShells.ci;
+        devShells.ci = inputs'.devshell.legacyPackages.mkShell {
+          packages = [pkgs.alejandra inputs'.mozilla-addons-to-nix.packages.default];
+          env = [
+            {
+              name = "DEVSHELL_NO_MOTD";
+              value = 1;
+            }
+          ];
         };
       };
     };
