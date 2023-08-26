15af9e7fb2555037504b555816eed3856edcf849
Chris Montgomery <chris@cdom.io>
2023-08-26 13:41:35 -0400

    feat(devshell): add commands to add/rm by slug

--- flake.nix
+++ flake.nix
@@ -35,6 +35,48 @@
           packages = [pkgs.alejandra inputs'.mozilla-addons-to-nix.packages.default];
           commands = [
             {
+              name = "add-addon";
+              category = "maintenance";
+              command = ''
+                SLUG="$1"
+                PNAME="''${2:-}"
+                MANIFEST="$PRJ_ROOT/src/addons.json"
+
+                cp "$MANIFEST" "$MANIFEST.tmp"
+                cat "$MANIFEST" \
+                  | ${pkgs.jq}/bin/jq \
+                      --arg slug "$SLUG" \
+                      --arg pname "$PNAME" \
+                      '. + [(
+                        {slug: $slug}
+                        + if ($pname != "")
+                          then {pname: $pname}
+                          else {} end
+                      )]
+                      | sort_by(.slug)' \
+                  > "''${MANIFEST}.tmp"
+                mv $MANIFEST.tmp $MANIFEST
+              '';
+            }
+            {
+              name = "rm-addon";
+              category = "maintenance";
+              help = "Remove an addon by its slug :: SYNOPSIS: <slug>";
+              command = ''
+                SLUG="$1"
+                MANIFEST="$PRJ_ROOT/src/addons.json"
+
+                cp "$MANIFEST" "$MANIFEST.tmp"
+                # TODO: return error if not found
+                cat "$MANIFEST" \
+                  | ${pkgs.jq}/bin/jq \
+                      --arg slug "$SLUG" \
+                      'map(select(.slug != $slug))' \
+                  > "''${MANIFEST}.tmp"
+                mv $MANIFEST.tmp $MANIFEST
+              '';
+            }
+            {
               name = "update-addons";
               category = "maintenance";
               help = "Update the addons listed in the manifest";
