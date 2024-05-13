1a41d0a3cad0f99527ff05711e7a37e2f77a4b3f
Chris Montgomery <chris@cdom.io>
2024-05-13 00:17:53 -0400

    refactor: move devshells to separate file

--- flake.nix
+++ flake.nix
@@ -21,6 +21,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.devshell.flakeModule
+        ./devshells.nix
         ./src
       ];
       systems = [
@@ -33,13 +34,7 @@
         _module.args.lib = inputs.nixpkgs.lib;
       };
       perSystem =
-        {
-          inputs',
-          config,
-          pkgs,
-          lib,
-          ...
-        }:
+        { inputs', pkgs, ... }:
         {
           _module.args.pkgs = inputs'.nixpkgs.legacyPackages;
 
@@ -50,93 +45,6 @@
           #     name = "all";
           #     paths = with code; [ app wasm ];
           #   };
-
-          devshells.default = {
-            devshell.name = "firefox-addons";
-            packages = [
-              inputs'.mozilla-addons-to-nix.packages.default
-
-              pkgs.fd
-              pkgs.jq
-              pkgs.nixfmt-rfc-style
-              pkgs.nodePackages.prettier
-            ];
-            commands = [
-              {
-                package = pkgs.just;
-                category = "tools";
-              }
-              {
-                name = "fmt";
-                category = "tools";
-                help = "format all (non-generated) nix files";
-                command = ''
-                  ${lib.getExe pkgs.fd} \
-                    --type file \
-                    --extension nix \
-                    --exclude '*.generated.*' \
-                    --exec-batch ${lib.getExe config.formatter} '{}'
-                '';
-              }
-              {
-                # TODO: accept moz url input
-                name = "add-addon";
-                category = "maintenance";
-                command = ''
-                  SLUG="$1"
-                  PNAME="''${2:-}"
-                  MANIFEST="$PRJ_ROOT/src/addons.json"
-
-                  cp "$MANIFEST" "$MANIFEST.tmp"
-                  cat "$MANIFEST" \
-                    | ${pkgs.jq}/bin/jq \
-                        --arg slug "$SLUG" \
-                        --arg pname "$PNAME" \
-                        '. + [(
-                          {slug: $slug}
-                          + if ($pname != "")
-                            then {pname: $pname}
-                            else {} end
-                        )]
-                        | sort_by(.slug)' \
-                    > "''${MANIFEST}.tmp"
-                  mv $MANIFEST.tmp $MANIFEST
-                '';
-              }
-              {
-                name = "rm-addon";
-                category = "maintenance";
-                help = "Remove an addon by its slug :: SYNOPSIS: <slug>";
-                command = ''
-                  SLUG="$1"
-                  MANIFEST="$PRJ_ROOT/src/addons.json"
-
-                  cp "$MANIFEST" "$MANIFEST.tmp"
-                  # TODO: return error if not found
-                  cat "$MANIFEST" \
-                    | ${pkgs.jq}/bin/jq \
-                        --arg slug "$SLUG" \
-                        'map(select(.slug != $slug))' \
-                    > "''${MANIFEST}.tmp"
-                  mv $MANIFEST.tmp $MANIFEST
-                '';
-              }
-              {
-                name = "update-addons";
-                category = "maintenance";
-                help = "Update the addons listed in the manifest";
-                command = ''
-                  mozilla-addons-to-nix $PRJ_ROOT/src/addons.json $PRJ_ROOT/src/addons.generated.nix
-                '';
-              }
-              {
-                name = "update-flake";
-                category = "maintenance";
-                help = "Update the flake dependencies";
-                command = "nix flake update";
-              }
-            ];
-          };
         };
     };
 }
